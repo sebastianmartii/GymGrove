9a609f6c83cd7214cfd930ec1c2bd36ab6dfe884
sebastianmartii <sebastian.marzec02@gmail.com>
2024-01-14 14:37:47 +0100

    move main activity, add android entry point annotation

--- app/src/main/java/com/example/gymgrove/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/gymgrove/presentation/main/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.gymgrove
+package com.example.gymgrove.presentation.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.gymgrove.ui.theme.GymGroveTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
