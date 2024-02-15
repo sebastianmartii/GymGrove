f3293900e7dd3a08b3be442039af61df12956057
sebastianmartii <sebastian.marzec02@gmail.com>
2024-02-15 20:25:05 +0100

    change status bar color to surface

--- app/src/main/java/com/example/gymgrove/ui/theme/Theme.kt
+++ app/src/main/java/com/example/gymgrove/ui/theme/Theme.kt
@@ -87,7 +87,7 @@ fun GymGroveTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
+            window.statusBarColor = colorScheme.surface.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
         }
     }
