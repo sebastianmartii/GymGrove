fb0803413a37355d70ed6c9758f4dc9d51db5d46
sebastianmartii <sebastian.marzec02@gmail.com>
2024-02-15 20:25:50 +0100

    implement reusable empty screen

---
+++ app/src/main/java/com/example/gymgrove/presentation/util/EmptyScreen.kt
@@ -0,0 +1,22 @@
+package com.example.gymgrove.presentation.util
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun EmptyScreen(message: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = message,
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
