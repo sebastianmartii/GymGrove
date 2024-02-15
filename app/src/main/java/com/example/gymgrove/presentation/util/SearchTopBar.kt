cfcc5f7e4ba8395bf73c54196f83a880479ab665
sebastianmartii <sebastian.marzec02@gmail.com>
2024-02-15 20:23:19 +0100

    implement reusable search bar

---
+++ app/src/main/java/com/example/gymgrove/presentation/util/SearchTopBar.kt
@@ -0,0 +1,152 @@
+package com.example.gymgrove.presentation.util
+
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.key
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+import com.example.gymgrove.R
+import kotlinx.collections.immutable.ImmutableList
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun SearchTopBar(
+    titleContent: @Composable () -> Unit = {},
+    searchQuery: String?,
+    onSearchQueryChange: (String?) -> Unit,
+    modifier: Modifier = Modifier,
+    actions: ImmutableList<AppBar.AppBarAction>,
+    placeholderText: String? = null,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() }
+) {
+    val focusRequester = remember { FocusRequester() }
+    val keyboardController = LocalSoftwareKeyboardController.current
+    val focusManager = LocalFocusManager.current
+
+    TopAppBar(
+        title = {
+            if (searchQuery == null) return@TopAppBar titleContent()
+
+            BasicTextField(
+                value = searchQuery,
+                onValueChange = onSearchQueryChange,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester)
+                    .showSoftKeyboard(remember { searchQuery.isEmpty() }),
+                textStyle = MaterialTheme.typography.titleMedium.copy(
+                    color = MaterialTheme.colorScheme.onBackground,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 18.sp,
+                ),
+                keyboardOptions = KeyboardOptions(
+                    imeAction = ImeAction.Search
+                ),
+                keyboardActions = KeyboardActions(
+                    onSearch = {
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
+                    }
+                ),
+                cursorBrush = SolidColor(MaterialTheme.colorScheme.onBackground),
+                singleLine = true,
+                decorationBox = { innerTextField ->
+                    TextFieldDefaults.DecorationBox(
+                        value = searchQuery,
+                        innerTextField = innerTextField,
+                        enabled = true,
+                        singleLine = true,
+                        visualTransformation = VisualTransformation.None,
+                        interactionSource = interactionSource,
+                        placeholder = {
+                            Text(
+                                text = placeholderText ?: stringResource(id = R.string.search_hint),
+                                modifier = Modifier.alpha(.78f),
+                                maxLines = 1,
+                                overflow = TextOverflow.Ellipsis,
+                                style = MaterialTheme.typography.titleMedium.copy(
+                                    fontSize = 18.sp,
+                                    fontWeight = FontWeight.Normal
+                                )
+                            )
+                        },
+                        container = {}
+                    )
+                }
+            )
+        },
+        navigationIcon = {
+            if (searchQuery != null) {
+                IconButton(onClick = {
+                    onSearchQueryChange(null)
+                    keyboardController?.hide()
+                    focusManager.clearFocus()
+                }) {
+                    Icon(
+                        imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = stringResource(id = R.string.cancel_search_action)
+                    )
+                }
+            }
+        },
+        actions = {
+            key("search") {
+                if (searchQuery == null) {
+                    IconButton(onClick = {
+                        onSearchQueryChange("")
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Search,
+                            contentDescription = stringResource(id = R.string.search_action)
+                        )
+                    }
+                } else if (searchQuery.isNotEmpty()) {
+                    IconButton(onClick = {
+                        onSearchQueryChange("")
+                        focusRequester.requestFocus()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Default.Close,
+                            contentDescription = stringResource(id = R.string.reset_search_query_action)
+                        )
+                    }
+                }
+            }
+            actions.onEach {
+                if (it.isDisplayed) {
+                    IconButton(onClick = { it.onClick() }) {
+                        Icon(imageVector = it.icon, contentDescription = it.title)
+                    }
+                }
+            }
+        },
+        modifier = modifier
+    )
+}
