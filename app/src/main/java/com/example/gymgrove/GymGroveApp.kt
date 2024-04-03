40c7fc6859359596f97e8d232ed2fae8f26bb429
sebastianmartii <sebastian.marzec02@gmail.com>
2024-04-03 20:19:51 +0200

    create channel for scheduled notifications

--- app/src/main/java/com/example/gymgrove/GymGroveApp.kt
+++ app/src/main/java/com/example/gymgrove/GymGroveApp.kt
@@ -1,7 +1,23 @@
 package com.example.gymgrove
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import com.example.gymgrove.data.notification.service.ScheduledNotificationServiceImpl
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GymGroveApp : Application()
+class GymGroveApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val channel = NotificationChannel(
+            ScheduledNotificationServiceImpl.CHANNEL_ID,
+            ScheduledNotificationServiceImpl.CHANNEL_NAME,
+            NotificationManager.IMPORTANCE_DEFAULT
+        )
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+}
