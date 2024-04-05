a2b2b0e394189253319527c2510e3f41f0de2f1d
sebastianmartii <sebastian.marzec02@gmail.com>
2024-04-05 20:44:31 +0200

    change workout model

--- app/src/main/java/com/example/gymgrove/domain/workout/model/Workout.kt
+++ app/src/main/java/com/example/gymgrove/domain/workout/model/Workout.kt
@@ -1,7 +1,11 @@
 package com.example.gymgrove.domain.workout.model
 
 data class Workout(
+    val id: Int,
     val name: String,
     val durationInMinutes: Int,
     val totalWorkingSets: Int,
+    val day: String,
+    val date: String,
+    val split: Int? = null,
 )
