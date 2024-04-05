d16a53f8837e4764f3ab966a10bc51e8b9817ded
sebastianmartii <sebastian.marzec02@gmail.com>
2024-04-05 20:45:03 +0200

    change exercise model

--- app/src/main/java/com/example/gymgrove/domain/exercise/model/Exercise.kt
+++ app/src/main/java/com/example/gymgrove/domain/exercise/model/Exercise.kt
@@ -1,15 +1,20 @@
 package com.example.gymgrove.domain.exercise.model
 
 data class Exercise(
+    val id: Int,
+    val workoutId: Int,
     val name: String,
-    val shortDescription: String,
-    val totalSets: Int,
+    val sets: Int,
     val totalRepsPerSet: Int,
+    val dateString: String,
+    val workingWeight: Float,
     // optional
+    val shortDescription: String? = null,
     val inBetweenSetsRestTimeInSeconds: Int? = null,
     val tempo: Tempo? = null,
-    val workingSets: Int? = null,
     val warmUpSets: Int? = null,
     val partialReps: Int? = null,
     val repsInReserve: Int? = null,
+    // drop set info, warm up sets info, anything not applicable above but important
+    val additionalInfo: String? = null
 )
